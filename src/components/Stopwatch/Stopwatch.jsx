fcb838e2ad7d5baf108dfbb7e37f6deba98418e6
nazar.kinash@voypost.com <nazar.kinash@voypost.com>
2021-03-03 20:09:34 +0200

    create stopwatch

--- src/components/Stopwatch/Stopwatch.jsx
+++ src/components/Stopwatch/Stopwatch.jsx
@@ -1,48 +1,41 @@
 import React, { useEffect, useState } from "react";
-import { interval, Subject } from "rxjs";
-import { scan, share, takeUntil } from "rxjs/operators";
-
+import { concat, interval, NEVER, of, Subject } from "rxjs";
+import { map, scan, share, switchMap, takeUntil } from "rxjs/operators";
 import "./style.css";
 
-const action$ = new Subject();
-action$.subscribe();
+const pause$ = new Subject();
+pause$.subscribe();
 
-const stopwatch$ = interval(1000)
+const stop$ = new Subject();
+stop$.subscribe();
+
+const pauser$ = pause$.pipe(scan((acc) => !acc, false));
+
+const starter$ = of(false);
+const stopwatch$ = concat(starter$, pauser$)
   .pipe(
-    takeUntil(action$),
-    scan((count) => count + 1)
+    switchMap((stopped) => (stopped ? NEVER : interval(1000))),
+    scan((acc) => acc + 1, 0),
+    map((v) => {
+      return {
+        hh: Math.floor(Math.floor(v / 60) / 60) % 60,
+        mm: Math.floor(v / 60) % 60,
+        ss: v % 60,
+      };
+    }),
+    takeUntil(stop$)
   )
   .pipe(share());
 
+const initialStateTime = { hh: 0, mm: 0, ss: 0 };
+
 const Stopwatch = () => {
-  const [sub, setSub] = useState(false);
-  const [stopwatchesList, setStopwatchesList] = useState([]);
-  let [hours, setHours] = useState(0);
-  let [minutes, setMinutes] = useState(0);
-  let [seconds, setSeconds] = useState(0);
-  let [counter, setCounter] = useState(0);
+  const [subscribtion, setSubscribtion] = useState(null);
   let [clicker, setClicker] = useState(0);
-
-  function renderTime() {
-    !(seconds % 3599) && seconds !== 0 && setHours(++hours);
-    !(seconds % 59) && seconds !== 0 && setMinutes(++minutes % 60);
-    setSeconds(++seconds % 60);
-  }
-
-  useEffect(() => {
-    sub && renderTime();
-  }, [counter]);
-
-  const startCount = () => {
-    setSub(
-      stopwatch$.subscribe(
-        (time) => {
-          setCounter(time + 1);
-        },
-        (err) => console.log("Err: ", err)
-      )
-    );
-  };
+  const [stopwatchesList, setStopwatchesList] = useState([]);
+  const [pause, setPause] = useState(false);
+  const [state, setState] = useState(initialStateTime);
+  const { hh, mm, ss } = state;
 
   useEffect(() => {
     setTimeout(() => {
@@ -50,43 +43,55 @@ const Stopwatch = () => {
     }, 300);
   }, [clicker]);
 
-  const stopCount = () => {
-    action$.next("stop");
-    setSub(false);
-  };
-
-  const resetState = () => {
-    setSeconds(0);
-    setMinutes(0);
-    setHours(0);
-  };
-
-  const startStopHandler = () => {
-    if (sub) {
-      stopCount();
-      setStopwatchesList([...stopwatchesList, { h: hours, m: minutes, s: seconds }]);
-      resetState();
-    } else {
-      startCount();
+  const startStop = () => {
+    if (pause) {
+      start();
+      return;
     }
+    if (hh || mm || ss) {
+      stop();
+      setStopwatchesList([...stopwatchesList, { hh, mm, ss }]);
+      setState(initialStateTime);
+      return;
+    }
+    start();
   };
-
-  const waitHandler = () => {
-    if (sub) {
+  const wait = () => {
+    if (hh || mm || ss) {
       setClicker(++clicker);
       if (clicker === 2) {
-        stopCount();
+        pause$.next();
+        setPause(true);
       }
     }
-    console.log(clicker);
   };
 
   const resetHandler = () => {
-    if (sub) {
-      action$.next("reset");
-      resetState();
-      startCount();
+    if (hh || mm || ss) {
+      stop();
+      setState(initialStateTime);
+      const sub = stopwatch$.subscribe({
+        next: (v) => setState(v),
+      });
+      setSubscribtion(sub);
     }
+  };
+
+  const start = () => {
+    console.log(subscribtion);
+    if (hh || mm || ss) {
+      pause$.next();
+      setPause(false);
+    } else if (!subscribtion) {
+      const sub = stopwatch$.subscribe({
+        next: (v) => setState(v),
+      });
+      setSubscribtion(sub);
+    }
+  };
+  const stop = () => {
+    stop$.next();
+    setSubscribtion(null);
   };
 
   return (
@@ -97,26 +102,26 @@ const Stopwatch = () => {
       <div className="stopwatch">
         <div>
           <div className="cd-box">
-            <p className="numbers hours">{hours < 10 ? `0${hours}` : hours}</p>
+            <p className="numbers hours">{hh < 10 ? `0${hh}` : hh}</p>
             <p className="strings timeRefHours">Hours</p>
           </div>
 
           <div className="cd-box">
-            <p className="numbers minutes">{minutes < 10 ? `0${minutes}` : minutes}</p>
+            <p className="numbers minutes">{mm < 10 ? `0${mm}` : mm} </p>
             <p className="strings timeRefMinutes">Minutes</p>
           </div>
 
           <div className="cd-box">
-            <p className="numbers seconds">{seconds < 10 ? `0${seconds}` : seconds}</p>
+            <p className="numbers seconds"> {ss < 10 ? `0${ss}` : ss}</p>
             <p className="strings timeRefSeconds">Seconds</p>
           </div>
         </div>
 
         <div className="btns-container">
-          <button onClick={startStopHandler} className="btn" id="start">
+          <button onClick={startStop} className="btn" id="start">
             Start/Stop
           </button>
-          <button onClick={waitHandler} className="btn" id="stop">
+          <button onClick={wait} className="btn" id="stop">
             Wait
           </button>
           <button onClick={resetHandler} className="btn" id="stop">
@@ -126,9 +131,9 @@ const Stopwatch = () => {
         <ol className="stopwatches-list">
           {!!stopwatchesList.length &&
             stopwatchesList.map((el, i) => (
-              <li key={i + 1 + "item"}>{`  ${el.h < 10 ? `0${el.h}` : el.h} : ${el.m < 10 ? `0${el.m}` : el.m} : ${
-                el.s < 10 ? `0${el.s}` : el.s
-              }`}</li>
+              <li key={i + 1 + "item"}>{`  ${el.hh < 10 ? `0${el.hh}` : el.hh} : ${
+                el.mm < 10 ? `0${el.mm}` : el.mm
+              } : ${el.ss < 10 ? `0${el.ss}` : el.ss}`}</li>
             ))}
         </ol>
       </div>
